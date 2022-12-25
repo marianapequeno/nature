43dfffa856d2361558f84365f408212a84a2f1c9
Mariana <marianapqueno@gmail.com>
2022-12-25 16:58:52 -0300

    Otimizações para melhorias do código e de performance devido aos eventos de scroll

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
+/* Debounce do Lodash */
+debounce = function(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
 /* Navegação por tabs */
 $("[data-group]").each(function () {
   const $allClick = $(this).find("[data-click]"), //ul li a
@@ -57,7 +73,7 @@ $("section").each(function () {
     id = $(this).attr("id"),
     $itemMenu = $('a[href="#' + id + '"]');
 
-  $(window).scroll(function () {
+  $(window).scroll(debounce(function () {
     const scrollTop = $(window).scrollTop();
 
     if (
@@ -68,7 +84,7 @@ $("section").each(function () {
     } else {
       $itemMenu.removeClass("active");
     }
-  });
+  }, 200));
 });
 
 /* Menu mobile */
@@ -78,54 +94,61 @@ $(".mobile-btn").click(function () {
 });
 
 /* Slide */
-function slider(sliderName, velocidade) {
-  let sliderClass = '.' + sliderName, 
-        activeClass = 'active', 
+(function () {
+  function slider(sliderName, velocidade) {
+    let sliderClass = "." + sliderName,
+      activeClass = "active",
+      rotate = setInterval(rotateSlide, velocidade);
+
+    $(sliderClass + " > :first").addClass(activeClass);
+
+    $(sliderClass).hover(
+      function () {
+        clearInterval(rotate);
+      },
+      function () {
         rotate = setInterval(rotateSlide, velocidade);
+      }
+    );
 
-  $(sliderClass + ' > :first').addClass(activeClass);
+    function rotateSlide() {
+      let activeSlide = $(sliderClass + " > ." + activeClass),
+        nextSlide = activeSlide.next();
 
-  $(sliderClass).hover(function() {
-    clearInterval(rotate);
-  }, function() {
-    rotate = setInterval(rotateSlide, velocidade);
-  });
+      if (nextSlide.length == 0) {
+        nextSlide = $(sliderClass + " > :first");
+      }
 
-  function rotateSlide() {
-    let activeSlide = $(sliderClass + ' > .' + activeClass),
-      nextSlide = activeSlide.next();
-
-    if (nextSlide.length == 0) {
-      nextSlide = $(sliderClass + ' > :first');
+      activeSlide.removeClass(activeClass);
+      nextSlide.addClass(activeClass);
     }
-
-    activeSlide.removeClass(activeClass);
-    nextSlide.addClass(activeClass);
   }
-}
-slider('introducao', 2000);
+  slider("introducao", 2000);
+})();
 
 /* Animação dos botões ao scroll */
-let $target = $('[data-anime="scroll"]'), 
-      animationClass = 'animate', 
-      offset = $(window).height() * 3/4;
+(function () {
+  let $target = $('[data-anime="scroll"]'),
+    animationClass = "animate",
+    offset = ($(window).height() * 3) / 4;
 
-function animeScroll() {
-  let documentTop = $(window).scrollTop();
+  function animeScroll() {
+    let documentTop = $(window).scrollTop();
 
-  $target.each(function() {
-    let itemTop = $(this).offset().top;
+    $target.each(function () {
+      let itemTop = $(this).offset().top;
 
-    if(documentTop > itemTop - offset) {
-      $(this).addClass(animationClass);
-    } else {
-      $(this).removeClass(animationClass);
-    }
-  });
-}
+      if (documentTop > itemTop - offset) {
+        $(this).addClass(animationClass);
+      } else {
+        $(this).removeClass(animationClass);
+      }
+    });
+  }
 
-animeScroll();
-
-$(document).scroll(function() {
   animeScroll();
-});
+
+  $(document).scroll(debounce(function () {
+    animeScroll();
+  }, 200));
+})();
